fbe7b4853c9990e53564feca421b501d32f43c49
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-01 02:26:39 +0100

    commit

--- 0x03-ES6_data_manipulation/.eslintrc.js
+++ 0x03-ES6_data_manipulation/.eslintrc.js
@@ -1,37 +1,37 @@
 module.exports = {
-  env: {
+	env: {
 	  browser: false,
 	  es6: true,
-	  jest: true
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:jest/recommended'
-  ],
-  globals: {
+	  jest: true,
+	},
+	extends: [
+	  'airbnb-base',
+	  'plugin:jest/all',
+	],
+	globals: {
 	  Atomics: 'readonly',
-	  SharedArrayBuffer: 'readonly'
-  },
-  parserOptions: {
-	  ecmaVersion: 2020,
-	  sourceType: 'module'
-  },
-  plugins: ['jest'],
-  rules: {
+	  SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+	  ecmaVersion: 2018,
+	  sourceType: 'module',
+	},
+	plugins: ['jest'],
+	rules: {
 	  'max-classes-per-file': 'off',
 	  'no-underscore-dangle': 'off',
 	  'no-console': 'off',
 	  'no-shadow': 'off',
 	  'no-restricted-syntax': [
-      'error',
-      'LabeledStatement',
-      'WithStatement'
-	  ]
-  },
-  overrides: [
+		'error',
+		'LabeledStatement',
+		'WithStatement',
+	  ],
+	},
+	overrides:[
 	  {
-      files: ['*.js'],
-      excludedFiles: 'babel.config.js'
+		files: ['*.js'],
+		excludedFiles: 'babel.config.js',
 	  }
-  ]
-};
+	]
+  };
