d01d7e5781c6925f29a4815ce489d81aa6508dcf
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-04 15:49:19 +0100

    add

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -4,7 +4,7 @@
  * @param {string} lastName - The last name of the user.
  * @returns {Promise} A Promise that resolves with an object containing the user's first name and last name.
  */
-function signUpUser (firstName, lastName) {
+function signUpUser(firstName, lastName) {
   return Promise.resolve({
     firstName,
     lastName
