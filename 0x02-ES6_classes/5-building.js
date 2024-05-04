8ac4932ec816e8144d53cd520bda7f2fe181d2cf
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-04 17:10:10 +0100

    add

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,17 @@
-class Building {
+export default class Building {
   constructor(sqft) {
+    if (new.target !== Building &&
+        this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
-  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
 
-  // Abstract method - must be overridden by subclasses
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  set sqft(value) {
+    this._sqft = value;
   }
 }
-
-export default Building;
