2a9a2091e714d6f16a1047106720ea6438476b28
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-04 16:29:02 +0100

    add

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,10 +1,6 @@
 
 import { uploadPhoto, createUser } from './utils.js';
 
-/**
- * Calls the uploadPhoto and createUser functions asynchronously and returns an object with their responses.
- * @returns {Object} An object with the responses from uploadPhoto and createUser functions, or an empty object if one of the async functions fails.
- */
 async function asyncUploadUser() {
   try {
     const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
