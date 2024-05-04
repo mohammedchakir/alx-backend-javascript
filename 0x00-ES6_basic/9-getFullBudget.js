e9c109c445ce37f7bc1f7a259daf9eb82852ba43
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-04 23:40:17 +0100

    add

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,14 +1,14 @@
 /* eslint-disable import/extensions */
 import getBudgetObject from './7-getBudgetObject.js';
 
-export default function getFullBudgetObject(income, gdp, capita) {
+export default function getFullBudgetObject (income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(income) {
+    getIncomeInDollars (income) {
       return `$${income}`;
     },
-    getIncomeInEuros(income) {
+    getIncomeInEuros (income) {
       return `${income} euros`;
     }
   };
