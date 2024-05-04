6971ddb9cc088696e326ac25f89e24b78e3ba8dc
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-04 17:03:26 +0100

    add

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency.js';
+
+class Pricing {
+  constructor (amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter and setter for amount
+  get amount () {
+    return this._amount;
+  }
+
+  set amount (newAmount) {
+    this._amount = newAmount;
+  }
+
+  // Getter and setter for currency
+  get currency () {
+    return this._currency;
+  }
+
+  set currency (newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  // Method to display full price format
+  displayFullPrice () {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert price
+  static convertPrice (amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
