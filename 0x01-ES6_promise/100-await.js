440fb73222a9b8bacf23f9ca2f3c319c7ec222cf
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-04 16:30:41 +0100

    add

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,19 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
 
-import { uploadPhoto, createUser } from './utils.js';
-
-async function asyncUploadUser() {
+export default async function asyncUploadUser() {
   try {
-    const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
-    return {
-      photo: photoResponse,
-      user: userResponse
-    };
-  } catch (error) {
-    return {
-      photo: null,
-      user: null
-    };
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    return { photo, user };
+  } catch (e) {
+    return { photo: null, user: null };
   }
 }
-
-export default asyncUploadUser;
