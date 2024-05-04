7ca7786b2ba5c5872df2014f4af159b77fa7039c
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-04 18:03:38 +0100

    add

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,20 @@
+import Car from './10-car.js';
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  // Override cloneCar method to return instance of Car
+  cloneCar() {
+    const constructor = Object.getPrototypeOf(this).constructor;
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
+
+export default EVCar;
