82e8c085f32429bdb030fa6192d4f208de943903
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-04 23:32:57 +0100

    add

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,14 +1,12 @@
-function getCurrentYear () {
+function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {
+  return {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
     [`capita-${getCurrentYear()}`]: capita
   };
-
-  return budget;
 }
