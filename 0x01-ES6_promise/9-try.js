a1e93b1bf3b76266ddfcbabc2fe1800db233cbc4
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-04 16:22:33 +0100

    add

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,8 +1,3 @@
-/**
- * Creates an array named queue and appends the result or error of executing the mathFunction to it.
- * @param {Function} mathFunction - The math function to execute.
- * @returns {Array} An array containing the result or error message and the message "Guardrail was processed".
- */
 export default function guardrail(mathFunction) {
   const queue = [];
   try {
