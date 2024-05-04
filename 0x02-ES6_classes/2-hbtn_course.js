c6f1241b39192aff48564fc0b71d8756e5235aa3
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-04 16:58:19 +0100

    add

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,51 +1,67 @@
-class HolbertonCourse {
+export default class HolbertonCourse {
   constructor(name, length, students) {
+    this._checkType(name, 'string', 'Name');
+    this._checkType(length, 'number', 'Length');
+    this._checkType(students, 'array', 'Students');
     this._name = name;
     this._length = length;
     this._students = students;
-
-    // Verify attribute types
-    if (typeof this._name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof this._length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
   }
 
-  // Getter and setter for name
   get name() {
     return this._name;
   }
 
-  set name(newName) {
-    if (typeof newName !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = newName;
+  set name(name) {
+    this._checkType(name, 'string', 'Name');
+    this._name = name;
   }
 
-  // Getter and setter for length
   get length() {
     return this._length;
   }
 
-  set length(newLength) {
-    if (typeof newLength !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    this._length = newLength;
+  set length(length) {
+    this._checkType(length, 'number', 'Length');
+    this._length = length;
   }
 
-  // Getter and setter for students
   get students() {
     return this._students;
   }
 
-  set students(newStudents) {
-    // You can add additional validation for students array here if needed
-    this._students = newStudents;
+  set students(students) {
+    this._checkType(students, 'array', 'Students');
+    students.forEach((student) => this._checkType(student, 'string', 'Students'));
+    this._students = students;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  _checkType(value, type, variableName, errorMessageParam) {
+    let errorMessage = errorMessageParam;
+    if (!errorMessage) {
+      switch (type) {
+        case 'string':
+          errorMessage = `${variableName} must be a string`;
+          break;
+        case 'number':
+          errorMessage = `${variableName} must be a number`;
+          break;
+        case 'array':
+          errorMessage = `${variableName} must be an array of strings`;
+          break;
+        default:
+          errorMessage = 'Invalid type';
+      }
+    }
+
+    if (type === 'array') {
+      if (!Array.isArray(value) || !value.every((item) => typeof item === 'string')) {
+        throw new TypeError(errorMessage);
+      }
+      // eslint-disable-next-line valid-typeof
+    } else if (typeof value !== type) {
+      throw new TypeError(errorMessage);
+    }
   }
 }
-
-export default HolbertonCourse;
