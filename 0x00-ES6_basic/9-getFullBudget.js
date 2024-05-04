2c399f536920f2d92f1505fc250478e2bd442cfd
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-04 02:04:19 +0100

    commit

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -5,7 +5,7 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const fullBudget = {
     ...budget,
     getIncomeInDollars: (income) => `$${income}`,
-    getIncomeInEuros: (income) => `${income} euros`,
+    getIncomeInEuros: (income) => `${income} euros`
   };
 
   return fullBudget;
