0faaadb3bacf384b92d497352caace2564b535fe
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-04 16:19:02 +0100

    add

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,14 @@
-export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve) => {
-    resolve({
-      firstName,
-      lastName
-    });
+/**
+ * Signs up a user with the provided first name and last name.
+ * @param {string} firstName - The first name of the user.
+ * @param {string} lastName - The last name of the user.
+ * @returns {Promise} A Promise that resolves with an object containing the user's first name and last name.
+ */
+function signUpUser(firstName, lastName) {
+  return Promise.resolve({
+    firstName,
+    lastName
   });
 }
+
+export default signUpUser;
