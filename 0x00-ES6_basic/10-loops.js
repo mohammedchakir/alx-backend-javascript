b8b1277a86aac2eba5bc714a848e4b7d960fc7dc
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-04 01:30:53 +0100

    commit

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
-export default function appendToEachArrayValue(array, appendString) {
-    const newArray = [];
-    for (const value of array) {
-      newArray.push(appendString + value);
-    }
-    return newArray;
+export default function appendToEachArrayValue (array, appendString) {
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
+  return newArray;
+}
