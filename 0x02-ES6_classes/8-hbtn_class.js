d920794162fc38810fedbeb74d801f297aadb24f
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-04 17:21:57 +0100

    add

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,26 +1,16 @@
-class HolbertonClass {
+export default class HolbertonClass {
   constructor(size, location) {
     this._size = size;
     this._location = location;
   }
 
-  get size() {
-    return this._size;
-  }
-
-  get location() {
-    return this._location;
-  }
-
   [Symbol.toPrimitive](hint) {
     if (hint === 'number') {
       return this._size;
-    } else if (hint === 'string') {
-      return this._location;
-    } else {
-      return this;
     }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return null;
   }
 }
-
-export default HolbertonClass;
