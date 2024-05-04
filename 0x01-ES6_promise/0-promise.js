ce7ff397c3dd968e9e45ac82cf9d031040edf3ce
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-04 14:50:03 +0100

    add

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,14 +1,3 @@
-/**
- * Simulates getting a response from an API.
- * @returns {Promise} A Promise that resolves after a short delay.
- */
-function getResponseFromAPI () {
-  return new Promise((resolve, reject) => {
-    // Simulating API response after a short delay (e.g., 1 second)
-    setTimeout(() => {
-      resolve('API response data');
-    }, 1000);
-  });
-}
-
-export default getResponseFromAPI;
+export default function getResponseFromAPI() {
+    return Promise.resolve(true);
+  }
