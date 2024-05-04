fce08757ae3cc7dc85d5b37b897121d214e86ad8
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-04 15:53:26 +0100

    add

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,8 @@
+/**
+ * Uploads a photo with the given filename.
+ * @param {string} filename - The name of the file to be uploaded.
+ * @returns {Promise} A Promise that rejects with an Error containing the message if the file cannot be processed.
+ */
+export default function uploadPhoto (filename) {
+  return Promise.reject(new Error(`${filename} cannot be processed`));
+}
