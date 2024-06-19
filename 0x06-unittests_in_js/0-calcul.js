4ebebb9a31289e89f8f89944dd1eba0fc5aa737d
mohammedchakir <zerroukimedchakir@gmail.com>
2024-06-19 15:41:28 +0100

    add files for corrections and adjustement

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,6 +1,5 @@
-// 0-calcul.js
-function calculateNumber (a, b) {
-  return Math.round(a) + Math.round(b);
+function calculateNumber(a, b) {
+    return Math.round(a) + Math.round(b);
 }
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,32 +1,21 @@
 const assert = require('assert');
+const {it, describe} = require('mocha');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('should return 4 when inputs are 1 and 3', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-
-  it('should return 5 when inputs are 1 and 3.7', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-
-  it('should return 5 when inputs are 1.2 and 3.7', () => {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-
-  it('should return 6 when inputs are 1.5 and 3.7', () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
-
-  it('should return 0 when inputs are 0 and 0', () => {
-    assert.strictEqual(calculateNumber(0, 0), 0);
-  });
-
-  it('should return -2 when inputs are -1 and -1', () => {
-    assert.strictEqual(calculateNumber(-1, -1), -2);
-  });
-
-  it('should return -1 when inputs are -0.5 and -0.5', () => {
-    assert.strictEqual(calculateNumber(-0.5, -0.5), -1);
-  });
+    it('checking if numbers round', () => {
+        assert.equal(calculateNumber(1, 3), 4);
+    });
+    it('checking if numbers round 2nd round', () => {
+        assert.equal(calculateNumber(1, 3.7), 5);
+    });
+    it('checking if numbers round 3nd round', () => {
+        assert.equal(calculateNumber(1.2, 3.7), 5);
+    });
+    it('checking if numbers round 4nd round', () => {
+        assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
+    it('checking negative return', () => {
+        assert.equal(calculateNumber(-1.3, -3.7), -5);
+      });
 });
