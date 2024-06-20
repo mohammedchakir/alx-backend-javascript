64c439030baf6fd67173839b6a77fd8864af20c3
mohammedchakir <zerroukimedchakir@gmail.com>
2024-06-20 15:54:37 +0100

    add

---
+++ 0x06-unittests_in_js/8-api/api.js
@@ -0,0 +1,13 @@
+// api.js
+const express = require('express');
+const app = express();
+
+const PORT = 7865;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
+});
---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,22 @@
+// api.test.js
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Index page', () => {
+  const url = 'http://localhost:7865/';
+
+  it('should return status code 200', (done) => {
+    request(url, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct result', (done) => {
+    request(url, (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
